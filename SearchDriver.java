01ed8b2863e02a7ed8f1ccf1be2bd6034872d922
dchen278 <reindeer914@gmail.com>
2021-12-18 19:08:27 -0500

    update searchdriver

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,54 +1,64 @@
 public class SearchDriver {
   public static void main(String[] args) {
-    SearchDriver driver = new SearchDriver();
-    // Comparable[] arr = fillArray(10);
-    // Comparable[] arr2 = fillArray(100);
-    // Comparable[] arr3 = fillArray(1000);
-    // Comparable[] arr4 = fillArray(10000);
-    // Comparable[] arr5 = fillArray(100000);
-    // Comparable[] arr6 = fillArray(1000000);
-    // Comparable[] arr7 = fillArray(10000000);
-    Comparable[] arr8 = fillArray(100000000);
-    // Comparable[] arr9 = fillArray(1000000000);
-    for (int i = 0; i < 3; i++) {
-      // Test best case, worst case, and regular case
-      System.out.println("Testing array of size " + arr8.length);
-      SOP("----Binary Search----");
-      System.out.print("Best case: " + driver.testBinSearch(arr8, 49_999_999));
-      System.out.print("Worst case: " + driver.testBinSearch(arr8, 0));
-      System.out.print("Regular case: " + driver.testBinSearch(arr8, 10000000));
-    }
+      SearchDriver driver = new SearchDriver();
+      Comparable[][] tests = new Comparable[][] {
+              fillArray(100),
+              fillArray(1000),
+              fillArray(10000),
+              fillArray(100000),
+              fillArray(1000000),
+              fillArray(10000000),
+              fillArray(100000000),
+      };
+      for (int i = 0; i < tests.length; i++) {
+          Comparable[] arr = tests[i];
+          SOP("\n");
+          SOP("-----------------Testing array of size " + arr.length + "-----------------");
+          SOP("----Binary Search----");
+          int bestCase = (0 + arr.length - 1) / 2;
+          System.out.print("Best case: " + driver.testBinSearch(arr, bestCase) + " ms");
+          System.out.print("Worst case: " + driver.testBinSearch(arr, 0) + " ms");
+          System.out.print("Regular case: " + driver.testBinSearch(arr, arr.length / 8) + " ms");
+          SOP("\n");
+          SOP("----Linear Search----");
+          System.out.print("Best case: " + driver.testLinSearch(arr, 0) + " ms");
+          System.out.print("Worst case: " + driver.testLinSearch(arr, arr.length) + " ms");
+          System.out.print("Regular case: " + driver.testLinSearch(arr, arr.length / 2) + " ms");
+
+          if (i < tests.length - 1) {
+              SOP("");
+          }
+      }
   }
+
   // test how fast linear search is
   public long testLinSearch(Comparable[] arr, int target) {
-    long _start = System.currentTimeMillis();
-    LinSearch.linSearch(arr, target);
-    long _end = System.currentTimeMillis();
-    long _time = _end - _start;
-    System.out.println("Linear search found target in " + _time + " ms");
-    return _time;
+      long _start = System.currentTimeMillis();
+      LinSearch.linSearch(arr, target);
+      long _end = System.currentTimeMillis();
+      long _time = _end - _start;
+      return _time;
   }
 
   // test how fast binary search is
   public long testBinSearch(Comparable[] arr, int target) {
-    long _start = System.currentTimeMillis();
-    BinSearch.binSearch(arr, target);
-    long _end = System.currentTimeMillis();
-    long _time = _end - _start;
-    System.out.println("Binary search found target in " + _time + " ms");
-    return _time;
+      long _start = System.currentTimeMillis();
+      BinSearch.binSearch(arr, target);
+      long _end = System.currentTimeMillis();
+      long _time = _end - _start;
+      return _time;
   }
 
   // fill array with random numbers
   public static Comparable[] fillArray(int size) {
-    Comparable[] arr = new Comparable[size];
-    for (int i = 0; i < size; i++) {
-      arr[i] = i;
-    }
-    return arr;
+      Comparable[] arr = new Comparable[size];
+      for (int i = 0; i < size; i++) {
+          arr[i] = i;
+      }
+      return arr;
   }
 
   public static void SOP(String s) {
-    System.out.println(s);
+      System.out.println(s);
   }
 }
