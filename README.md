51b47c00b8cf7f5f62ee332b206cc127f709d64a
dchen278 <reindeer914@gmail.com>
2021-12-18 19:57:08 -0500

    comment out regular case for now

--- README.md
+++ README.md
@@ -7,17 +7,18 @@
 </div>
 
 ## Methodology
-  * Test the best case, worst case, and regular case for each method
+  * Test the best case and worst case for each method
     * The best case for linear search is first element and the best case for binary is the element at median index
     * The worst case for for linear search is the last element and the worst case for binary is if the element is not in the array
   * We used a 2d array to store all the test cases
     * We then iterate over the 2d array to test
   * Utilized instance vars to store ms for each of the trials
     * After the 5 trials for each array, the time taken was averaged and displayed
+    * Instance vars would then be reset following each iteration of the main loop
 
 
 ## Results
-From arrays of size 10 to 100000, the times were negligible at 0ms. As the size of the arrays got larger, the times began to increase too. For an array size of 1 million, binary search took 0 ms while linear search took at most 0.2 ms. For an array size of 10,000,000, it took linear search at most 2.2 ms and took binary search 0.0 ms.
+From arrays of size 10 to 100000, the times were negligible at 0ms. As the size of the arrays got larger, the execution times began to increase too. For an array size of 1 million, binary search took 0 ms while linear search took at most 0.2 ms. For an array size of 10,000,000, it took linear search at most 2.2 ms and took binary search 0.0 ms.
 
 The most noticeable difference was for an array size of 100,000,000. The worst case for binary search took 0.0 ms, while the worst case for linear search took 22.8 ms.
 ## Conclusions
--- SearchDriver.java
+++ SearchDriver.java
@@ -29,22 +29,22 @@ public class SearchDriver {
             for (int j = 0; j < driver.trials; j++) {
                 driver.binaryBest += driver.testBinSearch(arr, bestCase);
                 driver.binaryWorst += driver.testBinSearch(arr, worstCase);
-                driver.binaryRegular += driver.testBinSearch(arr, arr.length / 8);
+                // driver.binaryRegular += driver.testBinSearch(arr, arr.length / 8);
 
                 driver.linearBest += driver.testLinSearch(arr, 0);
                 driver.linearWorst += driver.testLinSearch(arr, arr.length);
-                driver.linearRegular += driver.testLinSearch(arr, arr.length / 2); 
+                // driver.linearRegular += driver.testLinSearch(arr, arr.length / 2); 
             }
 
             SOP("----Binary Search----");
             print("Best case: " + calculateAvg(driver.binaryBest) + " ms \n");
             print("Worst case: " + calculateAvg(driver.binaryWorst) + " ms \n");
-            print("Regular case: " + calculateAvg(driver.binaryRegular) + " ms \n");
+            // print("Regular case: " + calculateAvg(driver.binaryRegular) + " ms \n");
             SOP("\n");
             SOP("----Linear Search----");
             print("Best case: " + calculateAvg(driver.linearBest) + " ms \n");
             print("Worst case: " + calculateAvg(driver.linearWorst) + " ms \n");
-            print("Regular case: " + calculateAvg(driver.linearRegular) + " ms \n");    
+            // print("Regular case: " + calculateAvg(driver.linearRegular) + " ms \n");    
             
             driver.reset();
             if (i < tests.length - 1) {
