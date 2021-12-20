d8c487cf32e53726edfd8693fe8b260eb8e6ad7e
dchen278 <dchen30@stuy.edu>
2021-12-20 12:46:57 -0500

    remove best case

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,6 +1,6 @@
 public class SearchDriver {
     private static int trials = 1000;
-    private long binaryBest, binaryWorst, linearBest, linearWorst = 0;
+    private long binaryWorst, linearWorst;
     
     public static void main(String[] args) {
         SearchDriver driver = new SearchDriver();
@@ -25,20 +25,16 @@ public class SearchDriver {
             SOP("\n");
             SOP("-----------------Testing array of length " + test.length + "-----------------");
             for (int j = 0; j < trials; j++) {
-                driver.binaryBest += driver.testBinSearch(test, bestCase);
                 driver.binaryWorst += driver.testBinSearch(test, worstCase);
 
-                driver.linearBest += driver.testLinSearch(test, 0);
                 driver.linearWorst += driver.testLinSearch(test, test.length);
             }
 
             SOP("----Binary Search----");
-            print("Best case: " + calculateAvg(driver.binaryBest) + " ms \n");
             print("Worst case: " + calculateAvg(driver.binaryWorst) + " ms \n");
 
             SOP("\n");
             SOP("----Linear Search----");
-            print("Best case: " + calculateAvg(driver.linearBest) + " ms \n");
             print("Worst case: " + calculateAvg(driver.linearWorst) + " ms \n");
             
             driver.reset();
@@ -85,10 +81,8 @@ public class SearchDriver {
     }
 
     public void reset() {
-        binaryBest = 0;
         binaryWorst = 0;
 
-        linearBest = 0;
         linearWorst = 0;
     }
 }
